68a565e9b466dc258e47f2fd9557de1bf8d34d8e
tsuixl <393248139@qq.com>
2018-06-30 22:31:20 +0800

    use cmd
    增加mac与window批处理文件

---
+++ ConvertCmd/Cmd/User/mac-run.sh
@@ -0,0 +1,4 @@
+
+dirname $0
+cd `dirname $0`
+./ConvertCmd "{\"ExcelPath\":\"../../../@ExcelTest\",\"DesPath\":\"../../../@OutputLua\",\"SelectFile\":false}"
---
+++ ConvertCmd/Cmd/User/win-run.bat
@@ -0,0 +1,3 @@
+cd Win64
+ConvertCmd.exe "{""ExcelPath"":""../../../@ExcelTest"",""DesPath"":""../../../@OutputLua"",""SelectFile"":false}"
+pause
